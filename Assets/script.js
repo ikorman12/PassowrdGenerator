e8be9d0a2009a10128ae3b952a36f5fb4ad44b42
Igor Korman <ikorman12@gmail.com>
2022-11-03 21:13:38 -0400

    added generatepw fn with for loop

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,10 @@
 // Assignment Code
+var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+var capitalAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var numbers = "0123456789".split("");
+var specialChar = "!?@#$%^&*()[]{}-_+=<>,./|:;`~'".split("");
+var possibleChar;
+var password = "";
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword ()
@@ -8,10 +14,25 @@ function generatePassword ()
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
+
+//generate the password matching the length and criteria
+function generatePassword (){
+    var password = "";
+    var length =getLength();
+    getCriteria();
+    //for loop to use the global varable above to generate a random pw
+    for(var i=0; i<length; i++){
+      var charInd = MAth.floor(Math.random() * possibleChar.length);
+      password += possibleChar[charInd];
+    }
+    return password;
+  }
+
+//Criteria for length
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
